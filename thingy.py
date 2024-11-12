4e1175e4db5eb3c95301cc6ca9d6f04d80b5d3b7
KachowProducts <98125745+KachowProducts@users.noreply.github.com>
2024-11-12 15:43:41 -0800

    Update thingy.py

--- thingy.py
+++ thingy.py
@@ -3,23 +3,25 @@ import urllib.request
 import streamlit as st
 
 headers = {
-    "Authorization": f"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6ImthcnNhYjM0M0BnbWFpbC5jb20iLCJGaXJzdE5hbWUiOiJLYXJ0aGlrIiwiSXNTaGFkb3ciOmZhbHNlLCJMYXN0TmFtZSI6IlNhYmhhbmF5YWthbSIsIl9pZCI6IjYxMTU0Nzg2MTQ2NzBjMDAwMTY3YjM5YyIsImV4cCI6MTczMTcwMjAwOCwianRpIjoiYjFkZGFiZjktYzFhNi00YmYwLWFjMjktODQzOGZhMWY3ZTVjIn0.vLhznXErGDxckeeaI6IUe8Kow041roW4AsUVrcKKce0"
+    "Authorization": f"Bearer {st.secrets["TOKEN"]}"
 }
 
-with open('blah.json', 'r') as file:
-    data = json.load(file)["Schools"]
-    schools = [{x["Name"]: x["Scid"]} for x in data]
-    # print(schools)
 
 def get_results(major, school, year):
-    id = ""
-    for x in schools:
-        for key in x:
-            if school in key:
-                id = x[key]
+    with open('blah.json', 'r') as file:
+        data = json.load(file)["Schools"]
+        data = [{x["Name"]: x["Scid"]} for x in data]
 
+    id = None
+    for x in data:
+        for key in x:
+            # print(key)
+            if school.lower() in key.lower():
+                id = x[key]
+                # print(id)
+                break
     URL = f"https://app.scoir.com/api/highschools/5e922e7f0925986577dd7b66/prospects/assessments?scid={id}&fromYear=2016&toYear=2024"
-    print(URL)
+    # print(URL)
     req = urllib.request.Request(URL, headers=headers)
     with urllib.request.urlopen(req) as response:
         data2 = json.loads(response.read())["Applications"]
@@ -28,13 +30,13 @@ def get_results(major, school, year):
         # print(major_applicants)
         accepted_applicants = [a for a in major_applicants if a.get("Accepted", False) == True]
         
-        st.write(f"Number of applicants who applied to that major: {len(major_applicants)}")
-        st.write(f"Number of those accepted: {len(accepted_applicants)}")
+        st.writeprint(f"Number of applicants who applied to that major: {len(major_applicants)}")
+        st.writeprint(f"Number of those accepted: {len(accepted_applicants)}")
         if(len(accepted_applicants) > 0):
             avg_un = sum(float(a.get("UnweightedGPA", 0)) for a in accepted_applicants) / len(accepted_applicants)
             avg_w = sum(float(a.get("WeightedGPA", 0)) for a in accepted_applicants) / len(accepted_applicants)
-            st.write(f"Average GPA {avg_un}/{avg_w}")
-            st.write(f"Percentage {len(accepted_applicants) / len(major_applicants)}")
+            st.writeprint(f"Average GPA {avg_un}/{avg_w}")
+            st.writeprint(f"Percentage {len(accepted_applicants) / len(major_applicants)}")
 
 
 st.title("Sandwich Stats")
