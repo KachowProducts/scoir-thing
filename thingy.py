15b6b40cec44e6e0309842e5babd604d58f62e22
KachowProducts <98125745+KachowProducts@users.noreply.github.com>
2024-11-12 12:40:15 -0800

    Update thingy.py

--- thingy.py
+++ thingy.py
@@ -1,23 +1,24 @@
 import json
 import streamlit as st
 
-def get_results(major, school):
+def get_results(major, school, year):
     with open('master.json', 'r') as file:
         data = json.load(file)["Applications"]
 
-        major_applicants = [a for a in data if major in a["Major"] and school in a["SchoolName"]]
+        major_applicants = [a for a in data if major in a["Major"] and school in a["SchoolName"] and a["GraduationYear"] >= year]
         accepted_applicants = [a for a in major_applicants if a.get("Accepted", False) == True]
 
         st.write(f"Number of applicants who applied to that major: {len(major_applicants)}")
         st.write(f"Number of those accepted: {len(accepted_applicants)}")
         st.write(f"Percentage {len(accepted_applicants) / len(major_applicants)}")
-        st.write([a["CounselorName"] for a in accepted_applicants])
+        # st.write([a["CounselorName"] for a in accepted_applicants])
 
-st.title("Sandwich Statistics")
-st.write("Made by Michael, Derek, and Karthik - BISV College Applicants (2023-2024)")
+st.title("Sandwich Stats")
+st.write("Made by Michael, Derek, and Karthik - BISV College Applicants (2016-2024)")
 major = st.text_input("Major", value="Computer Science")
 school = st.text_input("School", value="Berkeley")
+year = st.text_input("Year", value="2024")
 
 if st.button("Get Results"):
-    get_results(major, school)
+    get_results(major, school, year)
 
