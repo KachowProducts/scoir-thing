b554b77ab8fd589389a07b72a8c7b0badb0fb2f6
KachowProducts <98125745+KachowProducts@users.noreply.github.com>
2024-11-12 15:23:44 -0800

    Update thingy.py

--- thingy.py
+++ thingy.py
@@ -1,55 +1,46 @@
-import streamlit as st
 import json
-import requests
-import urllib.parse
-
-
-st.title("Scoir data viewer thingy!")
-st.write(
-    "placeholder"
-)
+import urllib.request
+import streamlit as st
 
 headers = {
     "Authorization": f"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6ImthcnNhYjM0M0BnbWFpbC5jb20iLCJGaXJzdE5hbWUiOiJLYXJ0aGlrIiwiSXNTaGFkb3ciOmZhbHNlLCJMYXN0TmFtZSI6IlNhYmhhbmF5YWthbSIsIl9pZCI6IjYxMTU0Nzg2MTQ2NzBjMDAwMTY3YjM5YyIsImV4cCI6MTczMTcwMjAwOCwianRpIjoiYjFkZGFiZjktYzFhNi00YmYwLWFjMjktODQzOGZhMWY3ZTVjIn0.vLhznXErGDxckeeaI6IUe8Kow041roW4AsUVrcKKce0"
 }
-# with open("Applications.json", "r") as file:
-#     data = json.load(file)
-# school_id = st.text_input("School ID: ", "")
-school_name = st.text_input("School Name: ", "", placeholder="University of California Santa Cruz")
-school_id = 0
-if st.button("Search for school"):
-    SEARCH_URL = f'https://app.scoir.com/api/search/college/{urllib.parse.quote(school_name)}?size=50'
-    response = requests.get(SEARCH_URL, headers=headers)
-    # st.write(response)
-    # st.write(SEARCH_URL)
-    if response:
-        data = response.json()
-        first_school = data[0]
-        school_id = first_school["scid"]
-        st.header(first_school["name"])
-    
-major = st.text_input("Major: ", "", placeholder="Computer Science")
-years = st.multiselect("Years: ",
-    [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024],
-    [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024]
-)
 
-if st.button("Filter"):
+with open('blah.json', 'r') as file:
+    data = json.load(file)["Schools"]
+    schools = [{x["Name"]: x["Scid"]} for x in data]
+    # print(schools)
 
-    URL = "https://app.scoir.com/api/highschools/5e922e7f0925986577dd7b66/prospects/assessments?scid={school_id}&fromYear=2016&toYear=2024&applicationType="
-    response = requests.get(URL, headers=headers)
-    if response:
-        data = response.json()
-        filtered_data = []
-        for app in data["Applications"]:
-            if major:
-                if school_name in app["SchoolName"] and major in app["Major"] and app["GraduationYear"] in years:
-                    filtered_data.append(app)
-            else:
-                if school_name in app["SchoolName"] and app["GraduationYear"] in years:
-                    filtered_data.append(app)
+def get_results(major, school, year):
+    id = ""
+    for x in schools:
+        for key in x:
+            if school in key:
+                id = x[key]
 
-
+    URL = f"https://app.scoir.com/api/highschools/5e922e7f0925986577dd7b66/prospects/assessments?scid={id}&fromYear=2016&toYear=2024"
+    print(URL)
+    req = urllib.request.Request(URL, headers=headers)
+    with urllib.request.urlopen(req) as response:
+        data2 = json.loads(response.read())["Applications"]
+        # print(data2)
+        major_applicants = [a for a in data2 if major in a["Major"] and a["GraduationYear"] == year]
+        # print(major_applicants)
+        accepted_applicants = [a for a in major_applicants if a.get("Accepted", False) == True]
         
-        st.write("Total Applications: ", len(filtered_data))
-        st.write(filtered_data)
+        st.write(f"Number of applicants who applied to that major: {len(major_applicants)}")
+        st.write(f"Number of those accepted: {len(accepted_applicants)}")
+        if(len(accepted_applicants) > 0):
+            avg_un = sum(float(a.get("UnweightedGPA", 0)) for a in accepted_applicants) / len(accepted_applicants)
+            avg_w = sum(float(a.get("WeightedGPA", 0)) for a in accepted_applicants) / len(accepted_applicants)
+            st.write(f"Average GPA {avg_un}/{avg_w}")
+            st.write(f"Percentage {len(accepted_applicants) / len(major_applicants)}")
+
+
+st.title("Sandwich Stats")
+st.write("Made by Michael, Derek, and Karthik - BISV College Applicants (2023-2024)")
+major = st.text_input("Major", value="Computer Science")
+school = st.text_input("School", value="Berkeley")
+year = st.number_input("Year", value=2024)
+if st.button("Get Results"):
+    get_results(major, school, year)
